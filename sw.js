a9fda2c05edbd6393bb9679fa5dfb117e396a66d
Productsandbarcode <irshadvkiomp2@gmail.com>
2026-06-20 07:22:07 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = "products-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./products-data.js"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(
+      response => response || fetch(event.request)
+    )
+  );
+});
